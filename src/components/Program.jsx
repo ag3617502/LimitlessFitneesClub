3e44ab679c5c9308e1f8d4db4df015d4241644e0
Kumar Gaurav <kumar.gurav@solutions2share.net>
2024-01-21 10:44:57 +0530

    changes:-added key

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Program.css";
-import {programsData} from "../data/programsData.js";
+import { programsData } from "../data/programsData.js";
 import rightArrow from "../assets/rightArrow.png"
 function Program(props) {
     return (
@@ -10,16 +10,16 @@ function Program(props) {
                 <span>PROGRAMS</span>
                 <span className='stroke-text'>TO SHAPE YOU</span>
             </div>
-            
+
             <div className="program-categories">
-                {programsData.map((program) => (
-                    <div className='category'>
+                {programsData.map((program, i) => (
+                    <div className='category' key={i}>
                         {program.image}
                         <span>{program.heading}</span>
                         <span>{program.details}</span>
                         <div className='join-now'>
                             <span>Join Now</span>
-                            <img src={rightArrow} alt=""/>  
+                            <img src={rightArrow} alt="" />
                         </div>
                     </div>
                 ))}
